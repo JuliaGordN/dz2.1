b94743ddef78f2715e54b8a577a2e01291ef9194
JuliaGordN <ms.vendystar@gmail.com>
2021-11-17 23:15:17 +0200

    *

--- dz2-http/client.js
+++ dz2-http/client.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const client = http.request({ host: '127.0.0.1', port: 1712 }, (response) => {
+  console.log(`statusCode: ${response.statusCode}`);
   let data = '';
   response.on('data', (chunk) => {
     data += chunk;
--- dz2-http/server.js
+++ dz2-http/server.js
@@ -1,11 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
-  response.write('Hello, dear friends!');
-  response.end('This is my homework');
+  if (request.url === '/' && request.method === 'GET') {
+    response.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
+    response.write('Hello, dear friends!');
+    response.end(' This is my homework. ');
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(JSON.stringify({ message: 'Nifiga ne rabotaet!! Valera, obyasni!!!!' }));
+  }
 });
-
 server.listen(1712, '127.0.0.1', () => {
   console.log('server is listening on 127.0.0.1:1234');
 });
